b8ba997107ae63c8dfbd4d6300b15df3c5038665
Alexsander Leal <alexsanderunr@gmail.com>
2025-01-16 16:20:06 -0300

    erro de sintaxe corrigido na linha 15 do javascript

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const inputItem = document.getElementById("input-item");
-let contador = 0;
 const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar = document.getElementById("adicionar-item");
+let contador = 0;
 
 botaoAdicionar.addEventListener("click", (evento) => {
     evento.preventDefault(); //não deixa atualizar a tela (previne um evento padrão como recarregar a página)
     if (inputItem.value === "") {
         alert("Prencha o Campo!");
-        return;
+        return
     }
 
     const itemDaLista = document.createElement("li");
     const containerItemDaLista = document.createElement("div");
-    containerItemDaLista.classList.add("lista-item-contrainer");
+    containerItemDaLista.classList.add("lista-item-container");
     const inputCheckbox = document.createElement("input");
     inputCheckbox.type = "checkbox";
     inputCheckbox.id = "checkbox-" + contador++;
