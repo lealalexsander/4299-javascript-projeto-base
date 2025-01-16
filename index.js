4010ef41165044e7b14f34b560e8411bd2e44ee2
Alexsander Leal <alexsanderunr@gmail.com>
2025-01-16 16:37:38 -0300

    data completa adicionada ao item da lista

--- index.js
+++ index.js
@@ -25,10 +25,14 @@ botaoAdicionar.addEventListener("click", (evento) => {
     itemDaLista.appendChild(containerItemDaLista);
     listaDeCompras.appendChild(itemDaLista);
 
+    const dataItem = document.createElement("p");
+    dataItem.classList.add("texto-data");
+    itemDaLista.appendChild(dataItem);
+
     const diaDaSemana = new Date().toLocaleDateString("pt-BR", {weekday: "long"});
     const data = new Date().toLocaleDateString("pt-Br");
     const hora = new Date().toLocaleTimeString("pt-Br", {hour: "numeric", minute: "numeric"});
 
     const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
-    console.log(dataCompleta);
+    dataItem.innerText = dataCompleta;
 })
