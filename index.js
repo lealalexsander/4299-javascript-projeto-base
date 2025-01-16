53572384a548d50938e15c44671fbc28204a3517
Alexsander Leal <alexsanderunr@gmail.com>
2025-01-16 16:33:42 -0300

    horas adicionada ao valor da data

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ botaoAdicionar.addEventListener("click", (evento) => {
 
     const diaDaSemana = new Date().toLocaleDateString("pt-BR", {weekday: "long"});
     const data = new Date().toLocaleDateString("pt-Br");
+    const hora = new Date().toLocaleTimeString("pt-Br", {hour: "numeric", minute: "numeric"});
 
-    const dataCompleta = `${diaDaSemana} (${data})`;
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
     console.log(dataCompleta);
 })
